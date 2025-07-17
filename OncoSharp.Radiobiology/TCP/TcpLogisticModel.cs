410bd22d629288689beaff773820ec648d92a126
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2025-07-17 11:02:42 +0200

    TcpLogisticModel added

--- OncoSharp.Radiobiology/TCP/TcpLogisticModel.cs
+++ OncoSharp.Radiobiology/TCP/TcpLogisticModel.cs
@@ -7,6 +7,7 @@
 using OncoSharp.Core.Quantities.CloudPoint;
 using OncoSharp.Core.Quantities.Dose;
 using OncoSharp.Core.Quantities.Probability;
+using OncoSharp.Radiobiology.GEUD;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,9 +17,11 @@ namespace OncoSharp.Radiobiology.TCP
 {
     public class TcpLogisticModel
     {
+        public Geud2GyModel GeudModel { get; }
+
         public double D50 { get; }
         public double Gamma50 { get; }
-
+        
 
         public TcpLogisticModel(double d50, double gamma50)
         {
@@ -26,9 +29,19 @@ namespace OncoSharp.Radiobiology.TCP
             Gamma50 = gamma50;
         }
 
-        public virtual ProbabilityValue ComputeVoxelResponse(EQD2Value eqd2)
+       
+        public virtual ProbabilityValue ComputeTcp(DoseCloudPoints<EQD2Value> points)
         {
-            var response = 1.0 + Math.Exp(4.0 * Gamma50 * (1.0 - eqd2.Value / D50));
+            return ComputeTcp(points.VoxelDoses);
+        }
+
+        public virtual ProbabilityValue ComputeTcp(IReadOnlyList<DoseCloudPoint<EQD2Value>> points)
+        {
+            if (points == null) throw new ArgumentNullException(nameof(points));
+            
+            var geud2Gy = GeudModel.Calculate(points);
+
+            var response = 1.0 + Math.Exp(4.0 * Gamma50 * (1.0 - geud2Gy.Value / D50));
             response = 1.0 / response;
 
             if (Double.IsNaN(response))
@@ -38,37 +51,8 @@ namespace OncoSharp.Radiobiology.TCP
             }
 
             return ProbabilityValue.New(response);
-        }
 
-        public virtual ProbabilityValue ComputeTcp(DoseCloudPoints<EQD2Value> points)
-        {
-            return ComputeTcp(points.VoxelDoses);
-        }
-
-        public virtual ProbabilityValue ComputeTcp(IReadOnlyList<DoseCloudPoint<EQD2Value>> points)
-        {
-            if (points == null) throw new ArgumentNullException(nameof(points));
-            ProbabilityValue tcp = ProbabilityValue.One;
-            var totalVolume = points.Select(p => p.Volume.Value).Sum();
             
-            foreach (var point in points)
-            {
-                var dose = point.Dose;
-                var volume = point.Volume;
-                var volumeFraction = volume.Value / totalVolume;
-
-                if (Math.Abs(volumeFraction) < 1E-16)
-                {
-                    tcp *= 1.0;
-                }
-                else
-                {
-                    var voxelResponse = ComputeVoxelResponse(point.Dose);
-                    tcp *= Math.Pow(voxelResponse.Value, volumeFraction);
-                }
-            }
-
-            return tcp;
         }
     }
 }
