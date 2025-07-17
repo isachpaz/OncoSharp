ab29a111df6838a8717ab7f01d31bd5720c678f1
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2025-07-17 11:01:53 +0200

    TcpNiemierkoModel added

---
+++ OncoSharp.Radiobiology/TCP/TcpNiemierkoModel.cs
@@ -0,0 +1,49 @@
+﻿// OncoSharp
+// Copyright (c) 2014 - 2025 Dr. Ilias Sachpazidis
+// Licensed for non-commercial academic and research use only.
+// Commercial use requires a separate license.
+// See https://github.com/isachpaz/OncoSharp for more information.
+
+using OncoSharp.Core.Quantities.CloudPoint;
+using OncoSharp.Core.Quantities.DimensionlessValues;
+using OncoSharp.Core.Quantities.Dose;
+using OncoSharp.Core.Quantities.Probability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OncoSharp.Radiobiology.GEUD;
+
+namespace OncoSharp.Radiobiology.TCP
+{
+    public class TcpNiemierkoModel
+    {
+        public Geud2GyModel GeudModel { get; }
+        public EQD2Value D50 { get; }
+        public GammaValue Gamma50 { get; }
+
+        public TcpNiemierkoModel(Geud2GyModel geudModel, EQD2Value d50, GammaValue gamma50)
+        {
+            GeudModel = geudModel;
+            D50 = d50;
+            Gamma50 = gamma50;
+        }
+        
+        public virtual ProbabilityValue ComputeTcp(DoseCloudPoints<EQD2Value> points)
+        {
+            return ComputeTcp(points.VoxelDoses);
+        }
+
+        public virtual ProbabilityValue ComputeTcp(IReadOnlyList<DoseCloudPoint<EQD2Value>> points)
+        {
+            if (points == null) throw new ArgumentNullException(nameof(points));
+            
+            var totalVolume = points.Select(p => p.Volume.Value).Sum();
+            
+            var geud2Gy = GeudModel.Calculate(points);
+            var d50DividedByGEUD2Gy = D50 / geud2Gy;
+            var tcp = ProbabilityValue.New(1.0 / (1.0 + Math.Pow(d50DividedByGEUD2Gy, 4 * Gamma50)));
+            
+            return tcp;
+        }
+    }
+}
